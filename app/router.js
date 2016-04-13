47b719d1b374688aebdcacf5a2497f94a2f1e0fe
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-13 10:47:19 -0500

    User can see new form

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('branches', {
+    path: '/'
+  }, function() {
+    this.route('new');
+  });
 });
 
 export default Router;
