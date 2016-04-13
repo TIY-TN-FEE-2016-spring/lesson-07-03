b9b9c528809b113d91d02ae21dfccee07e525546
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-13 12:03:41 -0500

    User can save new sale

---
+++ app/branches/detail/controller.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  newSale: {
+    saleDate: null,
+    price: ``,
+  },
+
+  saveSale(branch, attrs) {
+    // Create a new sale record
+    const sale = this.store.createRecord(`sale`, attrs);
+    sale.set(`branch`, branch);
+
+    // Save new sale record
+    sale.save().then(() => {
+      // Clear our form
+      this.set(`newSale`, {
+        saleDate: null,
+        price: ``,
+      });
+    });
+  },
+});
